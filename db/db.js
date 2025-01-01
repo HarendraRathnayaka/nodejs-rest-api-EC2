6de1e3731b1db9d96bb86d838742e4c77cf0c912
HarendraRathnayaka <127397899+HarendraRathnayaka@users.noreply.github.com>
2025-01-01 20:06:39 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -4,13 +4,21 @@ require("dotenv").config();
 
 const uri = process.env.MONGO_DB_URL;
 
+if (!uri) {
+  console.error("MongoDB URI is not defined. Check your environment variables.");
+  process.exit(1); // Exit with failure if URI is undefined
+}
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connection established");
   } catch (error) {
-    console.error("MongoDB connection failed", error);
-    process.exit(1);
+    console.error("MongoDB connection failed", error.message);
+    process.exit(1); // Exit with failure
   }
 };
 
